f3bdef87077a43ec9cf4b2013a07b4aa8e721e54
Tin La <tinla14@gmail.com>
2021-01-29 10:43:21 -0800

    Finished implement redux into react to display block and selecting block

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,10 +1,80 @@
-import React, { FC, Children } from 'react'
+import React, { FC, Children, useEffect, useCallback } from 'react';
+import useMousetrap from 'react-hook-mousetrap'
 
+// redux
+import { useDispatch, useSelector } from 'react-redux';
+import { AnyAction, Dispatch } from 'redux';
+import { createGrid, IReducer, selectBlock } from 'reducers';
+
+// types
+import { IBlock_Coords, INDEX } from 'typings';
+
+// components
 import Block from './block'
+
+// styles
 import { Container, Row } from './styles'
 
+interface IGridState {
+  selectedBlock?: IBlock_Coords
+}
+
 const Grid: FC = () => {
-  // Other stuff on the way
+  const state = useSelector<IReducer, IGridState>(({ selectedBlock }) => ({
+    selectedBlock,
+  }))
+  const dispatch = useDispatch<Dispatch<AnyAction>>()
+  const create = useCallback(() => dispatch(createGrid()), [dispatch])
+  useEffect(() => {
+    create()
+  }, [create])
+
+  // control blocks move with key up/down/left/right
+  function moveDown() {
+    if (state.selectedBlock && state.selectedBlock[0] < 8)
+      dispatch(
+        selectBlock([
+          (state.selectedBlock[0] + 1) as INDEX,
+          state.selectedBlock[1],
+        ])
+      )
+  }
+
+  function moveLeft() {
+    if (state.selectedBlock && state.selectedBlock[1] > 0)
+      dispatch(
+        selectBlock([
+          state.selectedBlock[0],
+          (state.selectedBlock[1] - 1) as INDEX,
+        ])
+      )
+  }
+
+  function moveRight() {
+    if (state.selectedBlock && state.selectedBlock[1] < 8)
+      dispatch(
+        selectBlock([
+          state.selectedBlock[0],
+          (state.selectedBlock[1] + 1) as INDEX,
+        ])
+      )
+  }
+
+  function moveUp() {
+    if (state.selectedBlock && state.selectedBlock[0] > 0)
+      dispatch(
+        selectBlock([
+          (state.selectedBlock[0] - 1) as INDEX,
+          state.selectedBlock[1],
+        ])
+      )
+  }
+
+  // bind keys and actions
+  useMousetrap('down', moveDown)
+  useMousetrap('left', moveLeft)
+  useMousetrap('right', moveRight)
+  useMousetrap('up', moveUp)
 
   return (
     <Container data-cy="grid-container">
@@ -13,7 +83,10 @@ const Grid: FC = () => {
           <Row data-cy="grid-row-container">
             {Children.toArray(
               [...Array(9)].map((_, colIndex) => (
-                <Block colIndex={colIndex} rowIndex={rowIndex} />
+                <Block
+                  colIndex={colIndex as INDEX}
+                  rowIndex={rowIndex as INDEX}
+                />
               ))
             )}
           </Row>
